bc01d0f74dee2dfc09953cc6cecc545ccf26b244
Rukaya <ruknehlawi@gmail.com>
2024-05-29 10:08:41 +0300

    l

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+  function openWebsite1() {
+    window.open("https://rukayanehlawi.github.io/Chocolate-store-website/", "_blank");
+  }
+
+  function openWebsite2() {
+    window.open("https://rukayanehlawi.github.io/Beauty-pharmacy/", "_blank");
+  }
+ function openWebsite3() {
+    window.open("https://rukayanehlawi.github.io/visit-japan/", "_blank");
+  }
+ function openWebsite4() {
+    window.open("https://rukayanehlawi.github.io/login-register/", "_blank");
+  }
